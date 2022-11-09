b4ee1c6be205dda89a81ab5ddb881f6ee888f523
aujjh521 <aujjh521@gmail.com>
2022-11-09 22:45:36 +0800

    notebook : Python Flask 網站後端開發 - MongoDB 取得資料

--- course_13_to/mongo_practice.py
+++ course_13_to/mongo_practice.py
@@ -8,12 +8,53 @@ import certifi #在連線到mongodb cluster的時候需要用到
 client = pymongo.MongoClient("mongodb+srv://aujjh521:1003@mycluster.iyd1nxs.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where()) #記得替換掉自己的密碼
 
 # 把資料放進資料庫中
-db = client.test #選擇操作 test 資料庫 (client物可以創建一個database, 這邊取名叫 test, 如果已經存在則使用該database)
+db = client.mywebsite #選擇操作 mywebsite 資料庫 (client物可以創建一個database, 這邊取名叫 mywebsite, 如果已經存在則使用該database)
 collection = db.users #選擇操作 users 集合 (在database裡面創建一個集合, 這邊取名叫 users, 如果已經存在則使用該集合)
-# 把資料新增到集合中
-collection.insert_one({
-    "name": "wcleeza",
-    "gender": "男"
-}) #mongodb裡面存的資料是json格式, json在python就是dict的概念
 
-print('資料新增成功!')
+# 把資料新增到集合中
+# result = collection.insert_one({
+#     "name": "丁滿",
+#     "email": "tin@tin.com",
+#     "password": "tin",
+#     "level":2
+# }) #mongodb裡面存的資料是json格式, json在python就是dict的概念
+
+# print('資料新增成功!')
+# print(f'該筆資料的id是{result.inserted_id}')
+
+
+
+# 把多筆資料新增到集合中
+# result = collection.insert_many([{
+#     "name": "彭",
+#     "email": "pp@pp.com",
+#     "password": "pp",
+#     "level":2
+# }, {
+#     "name": "嘎",
+#     "email": "gg@gg.com",
+#     "password": "gg",
+#     "level":3
+
+# }]) #mongodb裡面存的資料是json格式, json在python就是dict的概念
+
+# print('一次新增多筆資料成功!')
+# print(f'所有資料的id依序是{result.inserted_ids}')
+
+
+# 取得集合中的第一筆資料
+# data = collection.find_one()
+# print(data)
+
+# 根據ObjectId取得資料
+# from bson.objectid import ObjectId
+# data = collection.find_one(
+#     ObjectId('636a4b334f981cef195fca7f')
+# )
+# print(data)
+
+
+#取得集合中的所有文件
+cursor = collection.find()
+for doc in cursor:
+    print(doc)
